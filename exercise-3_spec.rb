30e298c5a0832ee24fe6306c9057c393a791b91c
Nikita <latinisnotdead07@gmail.com>
2020-05-12 12:28:59 +0100

    Completed Exercise 3.

---
+++ exercise-3_spec.rb
@@ -0,0 +1,21 @@
+# > remember_name[Rae]
+# => "Name stored!"
+# > recall_names
+# => [Rae]
+# > remember_name(Alec)
+# => "Name stored!"
+# > recall_names
+# => [Rae, Alec]
+
+
+describe "the remeber_name method" do
+  it "gives us a friendly message" do
+    expect(remember_name(Rae)).to eq "Name stored!"
+  end
+end
+
+describe "recall names" do
+  it "returns the array of names" do
+    expect(recall_names).to eq [Rae]
+  end
+end
